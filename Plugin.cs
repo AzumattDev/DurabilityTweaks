6372f7ea74bef0166be79fcfa84be0c52fb8f9fb
AzumattDev <azumattgaming@gmail.com>
2022-02-13 02:11:06 -0500

    Version bump 1.0.1

--- Plugin.cs
+++ Plugin.cs
@@ -17,7 +17,7 @@ namespace DurabilityTweaks
     public class DurabilityTweaksPlugin : BaseUnityPlugin
     {
         internal const string ModName = "DurabilityTweaks";
-        internal const string ModVersion = "1.0.0";
+        internal const string ModVersion = "1.0.1";
         private const string Author = "azumatt";
         private const string ModGUID = Author + "." + ModName;
         private static string ConfigFileName = ModGUID + ".cfg";
